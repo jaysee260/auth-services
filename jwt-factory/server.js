9b36e9b8a208a35c9bb3657b3a0b8cd1ffa0cc1b
Juan C. Gonzalez <javascriptdeveloper17@gmail.com>
2018-11-11 14:41:50 -0500

    left comments for self

--- jwt-factory/server.js
+++ jwt-factory/server.js
@@ -1,26 +1,25 @@
 const app = require("express")();
 const { name:projName = "Server" } = require("./package.json");
 const { PORT = 3000 } = process.env;
-const jwt = require("jsonwebtoken");
-const { dbConnection, checkWhitelist } = require("./checkWhitelist");
+
+const { jwt:jwtSettings } = require("./config");
+const { checkWhitelist, generateJwt } = require("./utils/auth");
 
 app.get("/", (req, res) => {
     res.status(200).send(true);
 });
 
 app.get("/api/jwt-factory", checkWhitelist, (req, res) => {
-    const {
-        issuer,
-        subject,
-        jwtid,
-        secret,
-        expiresIn
-     } = require("./config").jwt;
-    /** @todo make it so that not just anyone can get a token */
-    let payload = Date.now();
-    let token = jwt.sign({ payload }, secret, { issuer, subject, jwtid, expiresIn });
-
+    /** 
+     * if the whitelist check passses, we should attach a proper
+     * payload to the request object (jwtPayload).
+     * If program control has reached this endpoint, we can then
+     * assume there's a jwtPayload object attached to the request.
+     * We will pass that payload to generateJwt(settings, payload).
+     */
+    const token = generateJwt(jwtSettings);
     const msg = "This is where you can get a JWT.";
+
     res.status(200).send({ msg, token });
 });
 
