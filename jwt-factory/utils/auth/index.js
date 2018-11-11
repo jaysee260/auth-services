044f4cc391294438b9c61fe1f6100073ffe88255
Juan C. Gonzalez <javascriptdeveloper17@gmail.com>
2018-11-11 18:11:20 -0500

    working on email validation implementation

--- jwt-factory/utils/auth/index.js
+++ jwt-factory/utils/auth/index.js
@@ -38,6 +38,44 @@ exports.checkWhitelist = function (req, res, next) {
         });
 }
 
+exports.checkWhitelistPost = function (req, res, next) {
+    const { dbConnection } = require("../db");
+    /** @todo
+     * query DB.
+     * if email is found in whitelist, proceed ( next() ).
+     * otherwise, terminate request with appropriate status code and message.
+     */
+
+     const { email } = req.body;
+     res.send(email);
+    // console.log("initiating DB connection test...");
+    
+    //  dbConnection.authenticate()
+    //     .then(() => {
+    //         // do DB things.
+    //         console.log("DB connection successfull.");
+    //         console.log("This is where the whitelist would be checked.");
+    //         req.jwtPayload = null;
+    //         console.log("closing DB connection....");
+    //         dbConnection.close()
+    //             .then(() => {
+    //                 console.log("DB connection closed.");
+    //                 next();
+    //             })
+    //             .catch((error) => {
+    //                 console.log("Could not close DB connection...");
+    //                 console.log(error);
+    //                 res.status(505).send("Something went wrong.");
+    //             });
+            
+    //     })
+    //     .catch((error) => {
+    //         console.log("Attempt to establish connection with DB failed.");
+    //         console.log(error);
+    //         res.status(505).send("Something went wrong.");
+    //     });
+}
+
 exports.validatePayload = function (jwtPayload) {
 
 }
