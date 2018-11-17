e3e7ed48cdadca5531a5815396958b11d635b0b0
Juan C. Gonzalez <javascriptdeveloper17@gmail.com>
2018-11-17 15:52:57 -0500

    application pool is now opened on service startup. this way connection pool is shared application/service wide; controllers spawn requests off a single pool  and close them (each request) upon completion

--- myazure-idsrv/services/user-registration/server/index.js
+++ myazure-idsrv/services/user-registration/server/index.js
@@ -4,12 +4,24 @@ const { name } = require("../package.json");
 const PORT = 3000;
 const { userController } = require("./routes/controllers/userController");
 
-app.use(bodyParser.json());                                     
-app.use(bodyParser.urlencoded({ extended: true }));
+const { pool } = require("../utils/db");
 
 app.route("/api/user-registration")
     .post(userController.createUser)
 
-app.listen(PORT, () => {
+app.use(bodyParser.json());                                     
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT, async () => {
     console.log("%s running on port %s", name, PORT);
+    try
+    {
+        await pool.connect();
+        console.log("Connected to DB.");
+    }
+    catch (dbConnectionError)
+    {
+        console.log("Failed to connect to DB.");
+        console.log(dbConnectionError);
+    }
 })
