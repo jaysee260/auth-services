4aafb32ae56f8805307ecef8fdfde3f4f6a7b1fc
Juan C. Gonzalez <javascriptdeveloper17@gmail.com>
2018-11-17 16:10:03 -0500

    experimenting with certain pattern ideas

--- myazure-idsrv/services/user-registration/server/index.js
+++ myazure-idsrv/services/user-registration/server/index.js
@@ -1,30 +1,56 @@
-"use strict";
+(async (process) => {
+    "use strict";
 
-const app = require("express")();
-const bodyParser = require("body-parser");
-const { name } = require("../package.json");
-const PORT = 3000;
-const { userController } = require("./routes/controllers/userController");
+    const app = require("express")();
+    const { name : serviceName } = require("../package.json");
+    const { PORT = 3000 } = process.env;
 
-const { pool } = require("../utils/db");
+    // The application should have these things
+    // set up before it opens its gates to the world.
 
-app.route("/api/user-registration")
-    .post(userController.createUser);
+    // Middleware Registration
+    await (function registerMiddleWare(app) {
+        const bodyParser = require("body-parser");
 
-app.use(bodyParser.json());                                     
-app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(bodyParser.json());                                     
+        app.use(bodyParser.urlencoded({ extended: true }));
+    })(app);
 
-app.listen(PORT, async () => {
-    console.log("%s running on port %s", name, PORT);
-    // Service spawns its own connection pool.
-    try
-    {
-        await pool.connect();
-        console.log("Connected to DB.");
-    }
-    catch (dbConnectionError)
-    {
-        console.log("Failed to connect to DB.");
-        console.log(dbConnectionError);
-    }
-});
+
+    // Routes Registration
+    await (function registerRoutes(app) {
+        const { userController } = require("./routes/controllers/userController");
+
+        app.route("/api/user-registration")
+            .post(userController.createUser);
+
+    })(app);
+
+
+    // Database Initialization
+    await (async function establishDatabaseConnection() {
+        const { pool } = require("../utils/db");
+
+        // Service spawns its own connection pool.
+        try
+        {
+            await pool.connect();
+            console.log("Connected to DB.");
+        }
+        catch (dbConnectionError)
+        {
+            console.log("Failed to connect to DB.");
+            console.log(dbConnectionError);
+
+            console.log("Will not even attempt to start service.");
+            console.log("Process ending now with exit code 1.");
+            process.exit(1);
+        }
+
+    })();
+
+
+    app.listen(PORT, async () => {
+        console.log("%s running on port %s", serviceName, PORT);
+    });
+})(process)
