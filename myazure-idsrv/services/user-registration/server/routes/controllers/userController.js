bf86cb6d98642082021f613549c116978be4eb6d
Juan C. Gonzalez <javascriptdeveloper17@gmail.com>
2018-11-17 12:51:19 -0500

    refactored controller to use new orm and async/await

--- myazure-idsrv/services/user-registration/server/routes/controllers/userController.js
+++ myazure-idsrv/services/user-registration/server/routes/controllers/userController.js
@@ -1,55 +1,68 @@
 const { dbConnection } = require("../../../../../utils/db");
+const { pool } = require("../../../../../utils/db");
 
 exports.userController = {
-    createUser: function(req, res, next) {
-        console.log("createsUser route");
-        /** @todo insert user into Users table */
-        /** @todo verify user's email is added to whitespace
-         * after insertion into User's table.
-         */
-        dbConnection.authenticate()
-            .then(() => {
-                // do DB things.
-                console.log("DB connection successfull.");
-                
-                console.log("Building query...");
-                const { first_name, last_name, email, username, password } = req.body;
-                const query = `
-                    INSERT INTO Users (first_name, last_name, email, username, password)
-                    VALUES ('${first_name}', '${last_name}', '${email}', '${username}', '${password}')
-                `;
 
-                console.log("querying to DB...");
-                console.log("inserting User...");
-                dbConnection.query(query).spread((results, metadata) => {
-
-                    console.log("closing DB connection");
-                    dbConnection.close()
-                        .then(() => {
-                            console.log("DB connection closed.");
-                            console.log({ results });
-                            if (results.length === 1) {
-                                console.log(results);
-                                let msg = "User created successfully";
-                                res.status(200).json({ msg, results });
-                            } else {
-                               let msg = "Something went wrong when trying to create User.";
-                               res.status(505).json({ msg }); 
-                            }
-                        })
-                        .catch((error) => {
-                            console.log("Could not close DB connection...");
-                            console.log(error);
-                            res.status(505).send("Something went wrong.");
-                        })
-
-                });
-            })
-            .catch((error) => {
-                console.log("Could not close DB connection...");
-                console.log(error);
-                res.status(505).send("Something went wrong.");
-            })
+    createUser: async (req, res, next) => {
+        if ( Object.keys(req.body).length === 0 ) {
+            res.send("Request body cannot be empty.");
+            return;
+        }
         
-    }
+        
+        try
+        {
+            /** @todo
+             * On application start (at the myazure-idsrv level),
+             * open an application wide connection pool.
+             * 
+             * From controllers, spawn new requests from the pool.
+             * 
+             * In other words, this needs to move out of here
+             * and it instead needs to happen
+             */
+            await pool.connect();
+            console.log("connected to DB.");
+        }
+        catch (dbError)
+        {
+            console.log(dbError);
+            console.log("could not connect to DB.");
+        }
+
+        const { first_name, last_name, email, username, password } = req.body;
+        /** @todo add security against potential SQL injection */
+        console.log("building query...");
+        const userInsertQuery = `
+            INSERT INTO Users (first_name, last_name, email, username, password)
+            VALUES ('${first_name}', '${last_name}', '${email}', '${username}', '${password}')
+        `;
+
+        let result;
+        console.log("querying to DB...");
+        try
+        {
+            console.log("inserting User...");
+            result = await pool.query(userInsertQuery);
+
+            let msg = "User successfully created.";
+            console.log(msg);
+            console.log(result);
+            res.status(200).json({ msg })
+        }
+        catch (error) {
+            let msg = "An error occurred while trying to create new user.";
+            console.log(msg);
+            console.log(error);
+
+            res.status(500).json({ msg });
+        }
+        finally
+        {
+            pool.close();
+            console.log("DB connection closed.");
+        }
+
+    } // end of createUser
+
 }
