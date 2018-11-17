f2a19b631238e7a45949ff08ef4dbdc6671a075b
Juan C. Gonzalez <javascriptdeveloper17@gmail.com>
2018-11-17 17:13:28 -0500

    some refactoring

--- myazure-idsrv/services/jwt-factory/server/index.js
+++ myazure-idsrv/services/jwt-factory/server/index.js
@@ -5,7 +5,7 @@ const { PORT = 3000 } = process.env;
 const { pool } = require("../utils/db");
 
 const { jwt : jwtSettings } = require("../config");
-const { checkWhitelist, checkWhitelistPost, generateJwt } = require("../utils/auth");
+const { checkWhitelist, generateJwt } = require("../utils/auth");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,29 +15,7 @@ app.get("/jwt-factory", (req, res) => {
     res.status(200).send(true);
 });
 
-// Service API endpoint
-/** @deprecated */
-app.get("/api/jwt-factory", checkWhitelist, (req, res) => {
-    /** 
-     * if the whitelist check passses, we should attach a proper
-     * payload to the request object (jwtPayload).
-     * If program control has reached this endpoint, we can then
-     * assume there's a jwtPayload object attached to the request.
-     * We will pass that payload to generateJwt(settings, payload).
-     */
-    /** @todo validate payload before generating token '
-     * @method validatePayload(jwtPayload)?
-    */
-    // let payload = validateJwtPayload(req.jwtPayload);
-    let payload = req.jwtPayload ? req.jwtPayload : null;
-    const token = generateJwt(jwtSettings, payload);
-    const msg = "This is where you can get a JWT.";
-
-    res.status(200).send({ msg, token });
-});
-
-
-app.post("/api/jwt-factory", checkWhitelistPost, (req, res) => {
+app.post("/api/jwt-factory", checkWhitelist, (req, res) => {
     /** 
      * if the whitelist check passses, we should attach a proper
      * payload to the request object (jwtPayload).
@@ -54,7 +32,7 @@ app.post("/api/jwt-factory", checkWhitelistPost, (req, res) => {
      */
     
     const token = generateJwt(jwtSettings, jwtPayload);
-    const msg = "This is where you can get a JWT.";
+    const msg = req.whitelistCheckMsg; // attached inside checkWhitelist middleware method
     
     res.status(200).send({ msg, token });
 });
