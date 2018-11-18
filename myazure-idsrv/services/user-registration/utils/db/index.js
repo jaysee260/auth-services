007efdcad543fb231eabf480fba3dc224b9932e1
Juan C. Gonzalez <javascriptdeveloper17@gmail.com>
2018-11-18 10:55:28 -0500

    created utility method to build user creation query

--- myazure-idsrv/services/user-registration/utils/db/index.js
+++ myazure-idsrv/services/user-registration/utils/db/index.js
@@ -7,3 +7,22 @@ const { db : dbConfig } = require("../../../../config");
 
 
 exports.pool = new sql.ConnectionPool(dbConfig);
+
+exports.buildUserInsertQuery = async function (reqBody) {
+    const { hashPassword } = require("../auth");
+    const { first_name, last_name, email, username } = reqBody;
+    const hashedPassword = await hashPassword(reqBody.password);
+    delete reqBody.password;
+
+     /** NOTE:
+     * Per the docs:
+     *  > "All values are automatically sanitized against sql injection."
+     * 
+     * Reference: https://www.npmjs.com/package/mssql#connection-pools
+     *            under ES6 Tagged template literals.
+     */
+    return `
+        INSERT INTO Users (first_name, last_name, email, username, password)
+        VALUES ('${first_name}', '${last_name}', '${email}', '${username}', '${hashedPassword}')
+    `;
+}
